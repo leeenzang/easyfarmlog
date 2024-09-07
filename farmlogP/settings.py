bdb4e34afb6c94f98fd47e041d458f7003336b43
leeeunjin <enzn@hufs.ac.kr>
2024-09-07 15:11:50 +0900

    FEAT: heroku 연결

--- farmlogP/settings.py
+++ farmlogP/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()  # .env 파일을 로드
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wiv4_#x&1lck!&90gow)=+$4h$+wqb)s3e_kk+^8-vnse9hs0l"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
